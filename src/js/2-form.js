6970949c5fdaa47caab7165ead66f8941acc246c
Anna Kroka <avkroka.broker@gmail.com>
2024-09-07 13:45:09 -0700

    add gallery and form

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+let formData = {
+    email: "",
+    message: "",
+};
+
+const fillFormFields = () => {
+    const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (formDataFromLS === null) {
+        return;
+    }
+formData = formDataFromLS;
+
+console.log(formDataFromLS);
+console.dir(feedbackFormEl.elements);
+
+for (const key in formDataFromLS) {
+    if (formDataFromLS.hasOwnProperty(key)) {
+        feedbackFormEl.elements[key].value = formDataFromLS[key];
+    }
+}
+};
+
+const onFormFieldInput = event => {
+    const fieldName = event.target.name;
+    const fieldValue = event.target.value;
+
+    formData[fieldName] = fieldValue;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFeedbackFormSubmit = event => {
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem('feedback-form-state');
+};
+
+feedbackFormEl.addEventListener('input', onFormFieldInput);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
