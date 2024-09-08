db20409bca865521c187204422f334aec21e37ac
AnnaKroka <165294026+AnnaKroka@users.noreply.github.com>
2024-09-08 14:17:18 +0000

    Deploying to gh-pages from @ AnnaKroka/goit-js-hw-09@df2e42032e8285289737db3eabb8fc16a9979ac0 🚀

--- assets/2-form-DvD38g09.js
+++
@@ -1,2 +0,0 @@
-const a=document.querySelector(".feedback-form");let t={email:"",message:""};const m=e=>{const o=e.target.name,r=e.target.value;t[o]=r,localStorage.setItem("feedback-form-state",JSON.stringify(t))},n=e=>{e.preventDefault(),e.target.reset(),localStorage.removeItem("feedback-form-state")};a.addEventListener("input",m);a.addEventListener("submit",n);
-//# sourceMappingURL=2-form-DvD38g09.js.map
